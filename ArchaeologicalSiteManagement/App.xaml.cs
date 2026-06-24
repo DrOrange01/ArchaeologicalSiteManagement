9335b66a2c24fbce6e327f3c54f345f49aa6994e
DrOrange01 <20nikola01s@gmail.com>
2026-06-25 00:10:00 +0200

    Add project files.

---
+++ ArchaeologicalSiteManagement/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ArchaeologicalSiteManagement
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
